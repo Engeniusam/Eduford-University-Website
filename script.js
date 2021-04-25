a8c768df5001c69aec4cdf7845823780be20daa7
Samuel Macharia <engeniusam@gmail.com>
2021-04-25 21:09:05 +0300

    z-index for the darkmode icon

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function addDarkmodeWidget() {
     bottom: '64px', // default: '32px'
     right: 'unset', // default: '32px'
     left: '32px', // default: 'unset'
+    z-index: '10', //default: upper appearance
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
